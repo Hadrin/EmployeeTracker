c07d12e4087cdac53b24af8efeb3393b75feaa4c
David McGinley <dmstar9@gmail.com>
2022-05-07 22:39:07 -0400

    All queries implemented

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql2");
-const cTable = require('console.table');
+const cTable = require("console.table");
 
 let connection;
 
@@ -18,10 +18,10 @@ class sql {
   //Create tables if necessary
   create() {
     connection.query(
-      `CREATE TABLE IF NOT EXISTS department (id INT, name VARCHAR(30));`
+      `CREATE TABLE IF NOT EXISTS department (id INT PRIMARY KEY, name VARCHAR(30));`
     );
     connection.query(
-      `CREATE TABLE IF NOT EXISTS role (id INT, title VARCHAR(30), salary DECIMAL, department_id INT REFERENCES department(id));`
+      `CREATE TABLE IF NOT EXISTS role (id INT PRIMARY KEY, title VARCHAR(30), salary DECIMAL, department_id INT REFERENCES department(id));`
     );
     connection.query(
       `CREATE TABLE IF NOT EXISTS employee (id INT PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(30), role_id INT REFERENCES role(id), manager_id INT);`
@@ -31,25 +31,57 @@ class sql {
   //Next few methods display data from tables
   viewDepartments() {
     connection.query(`SELECT * FROM department;`, function (err, results) {
-      console.log(results);
+      console.clear();
+      console.log("Departments: ");
+      console.table(results);
+      console.log("Press any key to continue...");
     });
   }
   viewRoles() {
     connection.query(`SELECT * FROM role;`, function (err, results) {
-      console.log(results);
+      console.clear();
+      console.log("Roles: ");
+      console.table(results);
+      console.log("Press any key to continue...");
     });
   }
   viewEmployees() {
     connection.query(`SELECT * FROM employee;`, function (err, results) {
-      console.log(results);
+      console.clear();
+      console.log("Employees: ");
+      console.table(results);
+      console.log("Press any key to continue...");
     });
   }
+
+  //Create new entries in the database
+  createDepartment(name) {
+    connection.query(`INSERT INTO department (name) VALUES ('${name}');`);
+    this.viewDepartments();
+  }
+  createRole(title, salary, department_id) {
+    connection.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${title}', ${salary}, ${department_id});`);
+    this.viewRoles();
+  }
+  createEmployee(first, last, role, manager) {
+    connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${first}', '${last}', ${role}, ${manager});`);
+    this.viewEmployees();
+  }
+
+  //Updates an employee's role
+  updateEmployee(id, role){
+      connection.query(`UPDATE employee SET role_id = ${role} WHERE id = ${id}`);
+      this.viewEmployees();
+  }
+
 
   //Seeds default info into the database
   seed() {
     connection.query(`INSERT INTO department VALUES (1, 'engineering');`);
     connection.query(`INSERT INTO role VALUES (1, 'manager', 75000, 1);`);
-    connection.query(`INSERT INTO employee VALUES (1, 'The', 'Boss', 1, null);`);
+    connection.query(
+      `INSERT INTO employee VALUES (1, 'The', 'Boss', 1, null);`
+    );
   }
 }
 
--- server.js
+++ server.js
@@ -1,12 +1,65 @@
-const dotenv = require('dotenv').config();
-const sql = require('./db/queries');
-const mysql = require('mysql2');
+const dotenv = require("dotenv").config();
+const sql = require("./db/queries");
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
 
-const db = new sql
-console.log('test');
+const db = new sql();
+console.log("test");
 db.establishConnection();
 db.create();
-//db.seed();
-db.viewDepartments();
-db.viewRoles();
-db.viewEmployees();
+
+function inquirerLoop() {
+  inquirer
+    .prompt([
+      {
+        name: "menu",
+        type: "list",
+        choices: [
+          "View Departments",
+          "View Roles",
+          "View Employees",
+          "Create Department",
+          "Create Role",
+          "Create Employee",
+          "Update Employee",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.menu) {
+        case "View Departments":
+          db.viewDepartments();
+          inquirerLoop();
+          break;
+        case "View Roles":
+          db.viewRoles();
+          inquirerLoop();
+          break;
+        case "View Employees":
+          db.viewEmployees();
+          inquirerLoop();
+          break;
+        case "Create Department":
+          //TODO
+          inquirerLoop();
+          break;
+        case "Create Role":
+          //TODO
+          inquirerLoop();
+          break;
+        case "Create Employee":
+          //TODO
+          inquirerLoop();
+          break;
+        case "Update Employee":
+          //TODO
+          inquirerLoop();
+          break;
+        case "Exit":
+          break;
+      }
+    });
+}
+
+inquirerLoop();
